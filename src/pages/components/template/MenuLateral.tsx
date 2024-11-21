2950e00f61b36d56f3ed78a93502cab798687bc3
Bruno <brunoplay055@gmail.com>
2024-11-21 11:07:32 -0300

    feat: create logo

--- src/pages/components/template/MenuLateral.tsx
+++ src/pages/components/template/MenuLateral.tsx
@@ -1,10 +1,14 @@
 import { BellIcon, HomeIcon, SetIcon } from "../icons/icons";
+import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
   return (
     <div>
       <aside>
+        <div className="flex flex-col items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600 h-20 w-20">
+          <Logo />
+        </div>
         <ul>
           <MenuItem url="/" text="Inicio" icon={HomeIcon} />
           <MenuItem url="/ajustes" text="Ajustes" icon={SetIcon} />
